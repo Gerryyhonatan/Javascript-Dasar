b83b5eef07d053c85328e6b1b7eaef2f21610e05
gerry <gerryyhonatan29@gmail.com>
2025-01-22 13:31:19 +0700

    six commit

---
+++ latihanObject.js
@@ -0,0 +1,10 @@
+var mhs = {
+    nama : 'Sandhika',
+    umur : 31,
+    ips : [3.00,2.50,3.20],
+    alamat : {
+        jalan : 'JL. Kita masih panjang',
+        kota : 'Salatiga',
+        provinsi : 'Jawa Tengah'
+    }
+}
