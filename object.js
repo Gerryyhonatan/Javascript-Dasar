bc127eb87b6f005c40af759e666b2c3ac0b70204
gerry <gerryyhonatan29@gmail.com>
2025-01-25 14:02:37 +0700

    eleven commit

--- object.js
+++ object.js
@@ -1,64 +1,107 @@
-// OBJECT LITERAL PROBLEM: tidak efektif untuk object yang banyak
-// let mahasiswa = {
-//     nama: 'Sandhika',
-//     energi: 10,
-//     makan: function(porsi) {
-//         this.energi = this.energi + porsi;
-//         console.log(`Selamat datang ${this.nama}, selamat makan`);
-//     }
-// }
+/*
+// OBJECT LITERAL PROBLEM: tidak efektif untuk object yang banyak karena harus buat lagi namun nama tidak boleh sama
+let mahasiswa = {
+    nama: 'Sandhika',
+    energi: 10,
+    makan: function(porsi) {
+        this.energi = this.energi + porsi;
+        console.log(`Selamat datang ${this.nama}, selamat makan`);
+    }
+}
+
 
 // FUNCTION DECLARATION
-// const methodMahasiswa = {
-//     makan: function(porsi) {
-//         this.energi += porsi;
-//         console.log(`Halo ${this.nama}, Selamat makan`)
-//     },
+function Mahasiswa(nama,energi) {
+    let mahasiswa = {};
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+    
+    mahasiswa.makan = function (porsi) {
+        this.energi += porsi;
+        console.log(`Selamat datang ${this.nama}, selamat makan`);
+    }
+        return mahasiswa;
+    }
+        
+let sandhika = Mahasiswa('Sandhika', 20)
+let doddy = Mahasiswa('Doddy', 20)
 
-//     main: function (jam) {
-//         this.energi -= jam;
-//         console.log(`Halo ${this.nama}, Selamat bermain`)
-//     },
 
-//     tidur: function (jam) {
-//         this.energi += jam * 2;
-//         console.log(`Halo ${this.nama}, Selamat tidur`)
-//     }
-// }
+// CONSTRUCTOR FUNCTION
+function Mahasiswa(nama,energi) {
+    this.nama = nama;
+    this.energi = energi;
+           
+    this.makan = function(porsi) {
+        this.energi += porsi;
+        console.log(`Selamat datang ${this.nama}, selamat makan`);
+    }
+}
 
-// function Mahasiswa(nama, energi) {
-//     let mahasiswa = Object.create(methodMahasiswa);
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
+let sandhika = new Mahasiswa('Sandhika', 20)
 
-//     return mahasiswa;
-// }
 
-// let sandhika = Mahasiswa('Sandhika', 10);
+// FUNCTION DECLARATION YANG LEBIH EFEKTIF SECARA MANAJEMEN
+const methodMahasiswa = {
+    makan: function(porsi) {
+        this.energi += porsi;
+        console.log(`Selamat datang ${this.nama}, selamat makan`);
+    }
+}
+
+function Mahasiswa(nama, energi) {
+    let mahasiswa = {};
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+    mahasiswa.makan = methodMahasiswa.makan;
+
+    return mahasiswa;
+}
+
+let sandhika = Mahasiswa('Sandhika', 20);
+
+
+// FUNCTION DECLARATION LEBIH LEBIH EFEKTIF MENGGUNAKAN OBJECT.CREATE()
+const methodMahasiswa = {
+    makan: function(porsi) {
+        this.energi += porsi;
+        console.log(`Selamat datang ${this.nama}, selamat makan`);
+    }
+}
+
+function Mahasiswa(nama, energi) {
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+    
+    return mahasiswa;
+}
+
+let sandhika = Mahasiswa('Sandhika', 20);
 
 
 // VERSI PROTOTYPE
-// function Mahasiswa(nama, energi) {
-//     this.nama = nama;
-//     this.energi = energi;
-// }
+function Mahasiswa(nama, energi) {
+    this.nama = nama;
+    this.energi = energi;
+}
 
-// Mahasiswa.prototype.makan = function(porsi) {
-//     this.energi += porsi;
-//     return `Halo ${this.nama}, Selamat Makan`;
-// }
+Mahasiswa.prototype.makan = function(porsi) {
+    this.energi += porsi;
+    return `Halo ${this.nama}, Selamat Makan`;
+}
 
-// Mahasiswa.prototype.main = function (jam) {
-//     this.energi -= jam;
-//     return `Halo ${this.nama}, Selamat Bermain`;
-// }
+Mahasiswa.prototype.main = function (jam) {
+    this.energi -= jam;
+    return `Halo ${this.nama}, Selamat Bermain`;
+}
 
-// Mahasiswa.prototype.tidur = function (jam) {
-//     this.energi += jam * 2;
-//     return `Halo ${this.nama}, Selamat Tidur`;
-// }
+Mahasiswa.prototype.tidur = function (jam) {
+    this.energi += jam * 2;
+    return `Halo ${this.nama}, Selamat Tidur`;
+}
 
-// let sandhika = new Mahasiswa('Sandhika', 10);
+let sandhika = new Mahasiswa('Sandhika', 10);
 
 
 // VERSI CLASS
@@ -67,40 +110,22 @@ class Mahasiswa {
         this.nama = nama;
         this.energi = energi;
     }
-
+    
     makan(porsi) {
-    this.energi += porsi;
-    return `Halo ${this.nama}, Selamat Makan`;
+        this.energi += porsi;
+        return `Halo ${this.nama}, Selamat Makan`;
     }
-
+    
     main(jam) {
-    this.energi -= jam;
-    return `Halo ${this.nama}, Selamat Bermain`;
+        this.energi -= jam;
+        return `Halo ${this.nama}, Selamat Bermain`;
     }
-
+    
     tidur(jam) {
-    this.energi += jam * 2;
-    return `Halo ${this.nama}, Selamat Tidur`;
+        this.energi += jam * 2;
+        return `Halo ${this.nama}, Selamat Tidur`;
     }
 }
 
 let sandhika = new Mahasiswa('Sandhika', 10);
-
-
-// CONSTRUCTOR FUNCTION
-// function Mahasiswa(nama, energi) {
-//     this.nama = nama;
-//     this.energi = energi;
-
-//     this.makan = function(porsi) {
-//         this.energi += porsi;
-//         console.log(`Halo ${this.nama}, Selamat makan`)
-//     }
-
-//     this.main = function (jam) {
-//         this.energi -= jam;
-//         console.log(`Halo ${this.nama}, Selamat bermain`)
-//     }
-// }
-
-// let sandhika = new Mahasiswa('Sandhika', 10)
+*/
